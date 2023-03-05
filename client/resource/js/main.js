e2f8d3f24e0ecf9dc28a7d27fbab93da60238f82
What-ever-this-is <yuchestart@gmail.com>
2023-03-04 21:12:33 -0600

    d

--- client/resource/js/main.js
+++ client/resource/js/main.js
@@ -1,7 +1,8 @@
 const PARAMETERS = {
   HOST_ON_LOCAL_SERVER:true,//Aiden, change this to false when we actually deploy Splatshooter
   DEPLOYMENT_URL:"",
-  LOCAL_URL:"localhost:8080"
+  LOCAL_URL:"localhost:8080",
+  CLIENT_TYPE:"DEVELOPMENT" // DEVELOPMENT, DOWNLOADABLE, HOSTED
 }
 function main(){
   var renderer = new Renderer2D
