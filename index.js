6ed41e16697ff06286e3ac08a9059b6c7a48c53c
Neddslayer <neddslayer32@gmail.com>
2023-08-17 20:37:35 -0400

    remove unnecessary and unused things

--- index.js
+++ index.js
@@ -27,32 +27,26 @@ console.log("creating websocket server...");
 // Setup WSS
 const serverSocket = new ws.WebSocketServer({ server });
 
-let websockets = [];
-
-const matches = {};
-
 server.listen(config.port, (req, res) => {
     serverSocket.on('connection', (ws) => {
         ws.on('error', console.error);
         
         ws.on('message', (msg) => {
-            if (isCompressed(msg)) {
+            if (isCompressed(msg, true)) {
                 console.log("Recieved Message! Compressed data: " + msg)
                 const uncompressed = pako.inflate(msg, { to: 'string' });
                 console.log("Uncompressed data:" + uncompressed);
-            } else {
-                console.log("Recieved Message! Uncompressed data: " + msg)
             }
         });
     });
 })
 
-function isCompressed(data) {
+function isCompressed(data, intended) {
     try {
         pako.inflate(data);
         return true;
     } catch (error) {
-        console.warn("warning: uncompressed data caught!")
+        if (intended) console.warn("Warning: unintended uncompressed data caught! Data: " + data)
         return false;
     }
   }
@@ -60,33 +54,3 @@ function isCompressed(data) {
 console.log("Server listening on port " + config.port)
 
 console.log("Done.")
-// Setup WebSocket
-function websocketConnected(ws){
-    ws.on('message',recievedMessage)
-    ws.on('close',()=>{webSocketClose(ws)})
-    websockets.push(ws)
-    ws.id = websockets.length-1
-}
-
-function recievedMessage(message){
-    message = JSON.parse(message)
-    switch(message.type){
-        case "handshake":
-            this.send(messages.Message(
-                "handshake",
-                websockets.length-1
-            ))
-            break;
-        case "joinrequest":
-            console.log(`Join request recieved for ${message.senderId}`)
-            console.log(`Match requested: ${message.data.matchtype}`)
-            console.log(`Match ID Requested: ${message.data.matchid}`)
-            break;
-        case "matchtransformrequest":
-            break;
-    }
-}
-function webSocketClose(ws){
-    console.log(`Connection with socket #${ws.id} has been terminated.`)
-    websockets = websockets.filter(s => s!== ws)
-}
