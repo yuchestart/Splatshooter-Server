2b5f3dff6e143c66ceaeba1a139dddbf60cb1c3d
Neddslayer <neddslayer32@gmail.com>
2023-08-30 14:01:25 -0400

    Migrate some things, do some other things i don't fully remember what was done here

--- client/resource/js/network/socket.js
+++ client/resource/js/network/socket.js
@@ -8,6 +8,7 @@ export const CONNECTION = {
     parseMessage: function (message)
     {
         const uncompressed = JSON.parse(pako.inflate(message, { to: 'string' }));
+        console.log(uncompressed.data);
         switch (uncompressed.dataType)
         {
             case -1:
@@ -20,6 +21,11 @@ export const CONNECTION = {
                 break;
             case 1:
                 gameMain();
+                break;
+            case 2:
+                console.log("keep aldive")
+                const keepAlive = new Message(2, { id: uncompressed.data.id })
+                this.socket.send(keepAlive.compress());
                 break;
             default:
                 break;
@@ -36,7 +42,7 @@ export function INIT_CONNECTION()
     };
     CONNECTION.socket.addEventListener('open', (e) =>
     {
-        const handshake = new Message(0, { intent: 'query' }); // intent doesn't do anything (yet) but it's better than "Handshake start"
+        const handshake = new Message(0, { intent: 'login' });
         CONNECTION.socket.send(handshake.compress());
     })
     CONNECTION.socket.addEventListener('message', (e) =>
