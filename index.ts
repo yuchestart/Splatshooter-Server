a7af65e9e4c95ed46bca547619903f69ff8b3e1f
Neddslayer <neddslayer32@gmail.com>
2023-10-26 10:20:58 -0400

    remove client config options

--- index.ts
+++ index.ts
@@ -1,8 +1,4 @@
 /*
-MESSAGE TYPES:
--1: Error
-0: Handshake
-
 
 ERROR CATEGORIZATION:
 1xxx: Network
@@ -13,8 +9,6 @@ ERROR CATEGORIZATION:
 4xxx: Application errors
 4001: Client does not match version
 */
-//CLIENT
-import express from "express";
 //SERVER
 import config from "./splatshooter_config.json" assert {type: "json"};
 import https from "https";
@@ -30,76 +24,37 @@ export { LOGGER };
 const LOGGER = Util.getLogger("Splatshooter");
 
 // INIT SERVER
-if (config.server.host)
+LOGGER.info(`Splatshooter server - v${config.version}`);
+LOGGER.info("Loading...");
+
+LOGGER.info("Creating http server...");
+// setup http server
+let httpServer: http.Server<typeof http.IncomingMessage, typeof http.ServerResponse> | https.Server<typeof http.IncomingMessage, typeof http.ServerResponse>;
+
+if (config.use_ssl)
 {
-  LOGGER.info(`Splatshooter server - v${config.server.version}`);
-  LOGGER.info("Loading...");
-
-  LOGGER.info("Creating http server...");
-  // setup http server
-  let httpServer: http.Server<typeof http.IncomingMessage, typeof http.ServerResponse> | https.Server<typeof http.IncomingMessage, typeof http.ServerResponse>;
-
-  if (config.server.use_ssl)
-  {
-    httpServer = https.createServer({
-      cert: fs.readFileSync(`./${config.server.ssl.certificate}`),
-      key: fs.readFileSync(`./${config.server.ssl.key}`),
-    });
-  }
-  else
-  {
-    httpServer = http.createServer();
-  }
-  LOGGER.info("creating websocket server...");
-  // Setup WSS
-  const serverSocket = new WebSocketServer({ server: httpServer });
-  const splatserver = new SplatshooterServer(serverSocket);
-  httpServer.listen(config.server.port, () =>
-  {
-    splatserver.runServer();
-    httpServer.on("request", (req, res) =>
-    {
-      APIHandler.handleServer(req, res, splatserver);
-    });
+  httpServer = https.createServer({
+    cert: fs.readFileSync(`./${config.ssl.certificate}`),
+    key: fs.readFileSync(`./${config.ssl.key}`),
   });
-
-
-  LOGGER.info(`Server listening on port ${config.server.port}`);
 }
-
-if (config.client.host && config.server.host) LOGGER.info("-------------------------------------------");
-
-if (config.client.host)
+else
 {
-  LOGGER.info(`Splatshooter client - v${config.client.version}`);
-  LOGGER.info("Loading...");
-
-  const client_app = express();
-
-  client_app.use(express.static('client', {
-    setHeaders: (res, path) =>
-    {
-      // Set Cache-Control header to 'no-cache' for all static files
-      res.setHeader('Cache-Control', 'no-cache, no-store');
-    }
-  }));
-
-  let clientServer: http.Server<typeof http.IncomingMessage, typeof http.ServerResponse> | https.Server<typeof http.IncomingMessage, typeof http.ServerResponse>;
-
-  if (config.client.use_ssl)
-  {
-    clientServer = https.createServer({
-      cert: fs.readFileSync(`./${config.client.ssl.certificate}`),
-      key: fs.readFileSync(`./${config.client.ssl.key}`),
-    }, client_app);
-  }
-  else
-  {
-    clientServer = http.createServer(client_app);
-  }
-  clientServer.listen(config.client.port);
-
-
-  LOGGER.info(`Client listening on port ${config.client.port}`);
+  httpServer = http.createServer();
 }
+LOGGER.info("creating websocket server...");
+// Setup WSS
+const serverSocket = new WebSocketServer({ server: httpServer });
+const splatserver = new SplatshooterServer(serverSocket);
+httpServer.listen(config.port, () =>
+{
+  splatserver.runServer();
+  httpServer.on("request", (req, res) =>
+  {
+    APIHandler.handleServer(req, res, splatserver);
+  });
+});
+
+
+LOGGER.info(`Server listening on port ${config.port}`);
 LOGGER.info("Done.");
